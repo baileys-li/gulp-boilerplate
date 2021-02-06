a16be70c3a8fefc8ada218998d98461b49cf5d2d
Arthur <arthurlitovko@gmail.com>
2021-02-06 03:12:50 +0200

    Add gulp-if to styles

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,51 +5,48 @@ import stylelint from "gulp-stylelint";
 import autoprefixer from "gulp-autoprefixer";
 import shorthand from "gulp-shorthand";
 import clean from "gulp-clean-css";
+import gulpIf from "gulp-if";
 
 import { Path } from "../_const.js";
 
 export function styles() {
-	let cssFile = gulp.src(Path.SASS.source).pipe(
-		stylelint({
-			failAfterError: false,
-			reporters: [
-				{
-					formatter: "string",
-					console: true,
-				},
-			],
-		})
-	);
-
-	if (process.env.NODE_ENV === "development") {
-		cssFile = cssFile.pipe(sourcemaps.init());
-	}
-
-	cssFile = cssFile
+	return gulp
+		.src(Path.SASS.source)
+		.pipe(
+			stylelint({
+				failAfterError: false,
+				reporters: [
+					{
+						formatter: "string",
+						console: true,
+					},
+				],
+			})
+		)
+		.pipe(gulpIf(process.env.NODE_ENV === "development", sourcemaps.init()))
 		.pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
 		.pipe(
 			autoprefixer({
 				cascade: false,
 			})
 		)
-		.pipe(shorthand());
-
-	if (process.env.NODE_ENV === "production") {
-		cssFile = cssFile.pipe(
-			clean(
-				{
-					debug: true,
-					compatibility: "*",
-				},
-				(details) => {
-					console.log(
-						`${details.name}: Original size: ${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`
-					);
-				}
+		.pipe(shorthand())
+		.pipe(
+			gulpIf(
+				process.env.NODE_ENV === "production",
+				clean(
+					{
+						debug: true,
+						compatibility: "*",
+					},
+					(details) => {
+						console.log(
+							`${details.name}: Original size: ${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`
+						);
+					}
+				),
+				sourcemaps.write()
 			)
-		);
-	} else {
-		cssFile = cssFile.pipe(sourcemaps.write());
-	}
-	return cssFile.pipe(gulp.dest(Path.SASS.build));
+		)
+		.pipe(gulp.dest(Path.SASS.build));
 }

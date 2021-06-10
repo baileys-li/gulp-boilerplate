6bf6ed2c6c1cdd744354119ec8c9a49c5690d05e
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-06-11 01:18:58 +0300

    Change depoy task to GitHub Action

--- gulp/deployOnGHP.js
+++
@@ -1,6 +0,0 @@
-import ghpages from "gh-pages";
-import Path from "./_const.js";
-
-export function deployOnGitHubPages(cb) {
-	ghpages.publish(Path.PAGE.build, cb());
-}
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,7 @@ import { setDevelopment, setProduction } from "./gulp/setMode.js";
 import startProject from "./gulp/startProject.js";
 import serve from "./gulp/serve.js";
 import lint from "./gulp/lint.js";
-import { deployOnGitHubPages } from "./gulp/deployOnGHP.js";
 
 export const test = lint;
 export const build = gulp.series(setProduction, startProject);
 export default gulp.series(setDevelopment, startProject, serve);
-
-export const deploy = deployOnGitHubPages;

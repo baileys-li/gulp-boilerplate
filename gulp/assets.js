798019e7acbf723d2a203421bd7921206261b66b
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-06-11 05:29:50 +0300

    Copy public data to build

--- gulp/assets.js
+++ gulp/assets.js
@@ -1,6 +1,14 @@
 import gulp from "gulp";
 import Path from "./_const.js";
 
-export default function assets() {
+export default gulp.series(copyAssets, copyData);
+
+function copyAssets() {
 	return gulp.src(Path.ASSET.source).pipe(gulp.dest(Path.ASSET.build));
+}
+
+function copyData() {
+	return gulp
+		.src([Path.DATA.source, "!" + Path.DATA.hidden])
+		.pipe(gulp.dest(Path.DATA.build));
 }
